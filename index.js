ae03064d8b8e5e7abdadebd12e4d694686321e57
Anna Pojawis <annapojawis@gmail.com>
2018-11-28 17:01:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+let array = [ ];
+for(let i = 0; i < musicians.length; i++) {
+  array.push(musicians[i] + " plays " + instruments[i]);
+} 
+return array;
+}
+
+function johnLennonFacts(facts) {
+  let array = [ ];
+  let i = 0
+  while(i < facts.length) {
+    array.push(facts[i] + "!!!");
+    i++;
+}
+return array;
+}
+
+function iLoveTheBeatles(x) {
+  let array = [ ];
+  let i = 0;
+  do {
+    array = array + "I love the Beatles!"; 
+    i++;
+  }
+  while (i < (i-15));  
+  return array;
+}
